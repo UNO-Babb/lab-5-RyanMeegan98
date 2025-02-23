b6604cad757bfb997ddb323ccc19f4fbfed39677
RyanMeegan98 <ryan.meegan1171@gmail.com>
2025-02-23 16:59:32 +0000

    Completed

--- LetterFrequency.py
+++ LetterFrequency.py
@@ -1,7 +1,7 @@
 #LetterFrequency.py
-#Name:
-#Date:
-#Assignment:
+#Name:Ryan Meegan
+#Date:Feb 23 2025
+#Assignment:Letter Freq
 
 #This program will create a CSV file of frequencies based on a text file.
 #Use Excel or similar spreadsheet software to visualize the frequencies of the CSV file.
@@ -12,41 +12,34 @@ def countLetters(message):
     alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     message = message.upper()
 
-    freq = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    freq = [0] * 26  
 
-    #loop through each letter
-    #Find the position in the alphabet
-    #Increase the frequency at that position. If position was 5, then frequencies[5] = frequencies[5] + 1
+    
+    for letter in message:
+        if letter in alpha:  
+            index = alpha.index(letter)  
+            freq[index] += 1  
 
-
-
-    #Create the output text in the format A,5\n if there were 5 letter A in the message.
-    #Remember that the \n is the symbol for a new line.
-
-    output = ""
+    
+    output = "Letter,Frequency\n"  
     for i in range(26):
-        print (alpha[i], ":", freq[i])
-        line = alpha[i] + "," + str(freq[i]) + "\n"
-        output = output + line
+        print(alpha[i], ":", freq[i])  
+        output += f"{alpha[i]},{freq[i]}\n"  
 
     writeToFile(output)
-
 
 def writeToFile(fileText):
     dir_path = os.path.dirname(os.path.realpath(__file__))
     os.chdir(dir_path)
 
-    freqFile = open("frq.csv", 'w')
-    freqFile.write(fileText)
+    with open("frq.csv", 'w') as freqFile:
+        freqFile.write(fileText)
 
-    freqFile.close()
-
+    print("Frequency data saved to frq.csv")
 
 def main():
     msg = input("Enter a message: ")
     countLetters(msg)
 
-
-
 if __name__ == '__main__':
-  main()
+    main()
